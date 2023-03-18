1169a57431b490858f5bcb682ae4f8f7b869e788
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-03-18 23:59:37 +0800

    decorate board

--- game.cpp
+++ game.cpp
@@ -11,35 +11,43 @@ int main(){
         {"7","8","9"}
     };
     //output field
+    cout<<"---------";
     for(int i=0;i<3;i++){
         cout<<endl;
         for(int j=0;j<3;j++){
             cout<<arr[i][j]<<"   ";
         }
     }
+    cout<<"\n---------";
+
     
     do{
         //get input
         cout<<endl<<"enter the number to fill O:";
         cin>>temp;
         arr[temp/3][temp%3-1]="O";
-    
+        
+        cout<<"---------";
         for(int i=0;i<3;i++){
             cout<<endl;
             for(int j=0;j<3;j++){
                 cout<<arr[i][j]<<"   ";
             }
         }
-    
+        cout<<"\n---------";
+
         cout<<endl<<"enter the number to fill X:";
         cin>>temp;
         arr[temp/3][temp%3-1]="X";
+
+        cout<<"---------";
         for(int i=0;i<3;i++){
             cout<<endl;
             for(int j=0;j<3;j++){
                 cout<<arr[i][j]<<"   ";
             }
         }
+        cout<<"\n---------";
 
         
         
